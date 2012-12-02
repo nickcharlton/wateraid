7c42e02be7e4129e897708298fb77b90d5c5b52e
Nick Charlton <hello@nickcharlton.net>
2012-12-02 02:38:42 +0000

    Switches the feature slider to a contained view controller.

---
+++ WaterAid/WAFeatureSliderController.h
@@ -0,0 +1,13 @@
+//
+//  WAFeatureSliderController.h
+//  WaterAid
+//
+//  Created by Nick Charlton on 02/12/2012.
+//  Copyright (c) 2012 Nick Charlton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAFeatureSliderController : UIViewController
+
+@end
---
+++ WaterAid/WAFeatureSliderController.m
@@ -0,0 +1,27 @@
+//
+//  WAFeatureSliderController.m
+//  WaterAid
+//
+//  Created by Nick Charlton on 02/12/2012.
+//  Copyright (c) 2012 Nick Charlton. All rights reserved.
+//
+
+#import "WAFeatureSliderController.h"
+
+@interface WAFeatureSliderController ()
+
+@end
+
+@implementation WAFeatureSliderController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
