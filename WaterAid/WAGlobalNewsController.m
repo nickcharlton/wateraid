420bf121bbf2704433a5c96ad623e3f943fb233f
Nick Charlton <hello@nickcharlton.net>
2012-12-02 04:08:43 +0000

    Adds the global news section.

---
+++ WaterAid/WAGlobalNewsController.m
@@ -0,0 +1,20 @@
+//
+//  WAGlobalNewsController.m
+//  WaterAid
+//
+//  Created by Nick Charlton on 02/12/2012.
+//  Copyright (c) 2012 Nick Charlton. All rights reserved.
+//
+
+#import "WAGlobalNewsController.h"
+
+@implementation WAGlobalNewsController
+
+- (void)viewDidLoad
+{
+    // set the title sizes
+    UIFont *font = [UIFont fontWithName:@"MetaOffc-Norm" size:20.0f];
+    [self.titleLabel setFont:font];
+}
+
+@end
