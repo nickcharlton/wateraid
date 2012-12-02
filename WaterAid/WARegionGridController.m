7ac2f122d559c35b7025f5365d4b867332ca585b
Nick Charlton <hello@nickcharlton.net>
2012-12-02 03:09:06 +0000

    Sets the font colour and size for the region grid.

--- WaterAid/WARegionGridController.m
+++ WaterAid/WARegionGridController.m
@@ -10,4 +10,21 @@
 
 @implementation WARegionGridController
 
+- (void)viewDidLoad
+{
+    // set the title sizes
+    UIFont *font = [UIFont fontWithName:@"MetaOffc-Norm" size:20.0f];
+    [self.centralAmericaLabel setFont:font];
+    [self.asiaLabel setFont:font];
+    [self.africaLabel setFont:font];
+    [self.westPacificLabel setFont:font];
+    
+    // set the title colours
+    UIColor *color = [UIColor colorWithRed:0.0f green:0.682352941f blue:0.937254902f alpha:1.0];
+    self.centralAmericaLabel.textColor = color;
+    self.asiaLabel.textColor = color;
+    self.africaLabel.textColor = color;
+    self.westPacificLabel.textColor = color;
+}
+
 @end
